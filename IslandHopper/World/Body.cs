d8edca8daaea4a8fcab073cf3e19614db836dfe9
INeedAUniqueUsername <achen115@ucr.edu>
2018-12-26 01:12:19 -0800

    Prepare to implement Armors

--- IslandHopper/World/Body.cs
+++ IslandHopper/World/Body.cs
@@ -5,31 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace IslandHopper {
-	interface IBodyPart {
+	public interface IBodyPart {
 		EBodyPartType Type { get; }
 		string Name { get; }
 		int MaxHP { get; }
 		int CurrentHP { get; set; }
 		int Bleeding { get; set; }
-		Item Equipped { get; set; }
+		HashSet<Item> Equipped { get; set; }
 	}
-	interface Body {
+	public static class BodyHelper {
+		//Equipped items are kept in the parent entity's inventory
+		//If we removed any items from our inventory, make sure they get removed from our equips
+		public static void UpdateEquipped(this IBodyPart bp, HashSet<Item> Inventory) {
+			bp.Equipped.RemoveWhere(i => !Inventory.Contains(i));
+		}
+		/*
+		public static bool CanEquip(this IBodyPart bp, Item i) {
+			switch(bp.Type) {
+				case EBodyPartType.Head: return bp.Equipped.FirstOrDefault(e => e.Head != null);
+				case EBodyPartType.Arm: return bp.Equipped.FirstOrDefault(e => e.Arm != null);
+				case EBodyPartType.Leg: return bp.Equipped.FirstOrDefault(e => e.Leg != null);
+			}
+		}
+		*/
+	}
+	public interface Body {
 		Dictionary<EBodyPartType, IBodyPart> parts { get; }
 	}
-	enum EBodyPartType {
+	public enum EBodyPartType {
 		Head,
 		Arm,
 		Torso,
 		Leg,
 	}
-	class BodyPart : IBodyPart {
+	public class BodyPart : IBodyPart {
 		public EBodyPartType Type { get; }
 		public string Name { get; private set; }
 		public int MaxHP { get; private set; } = 100;
 		public int CurrentHP { get; set; } = 100;
 		public int Bleeding { get; set; } = 0;
-		public Item Equipped { get; set; } = null;
-		private static BodyPart CreateStandardPart(EBodyPartType type, string Name) => new BodyPart() { Name = Name, MaxHP = 100, CurrentHP = 100, Equipped = null };
+		public HashSet<Item> Equipped { get; set; } = null;
+		private static BodyPart CreateStandardPart(EBodyPartType type, string Name) => new BodyPart() { Name = Name, MaxHP = 100, CurrentHP = 100, Equipped = new HashSet<Item>() };
 		private static BodyPart CreateHead() => CreateStandardPart(EBodyPartType.Head, "Head");
 		private static BodyPart CreateLeftArm() => CreateStandardPart(EBodyPartType.Arm, "Left Arm");
 		private static BodyPart CreateRightArm() => CreateStandardPart(EBodyPartType.Arm, "Right Arm");
