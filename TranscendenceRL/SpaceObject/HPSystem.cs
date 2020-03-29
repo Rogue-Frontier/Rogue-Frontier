171fde29706110e2e21f01bfde0c81e4325c0a50
Alex Chen <achen115@ucr.edu>
2020-03-28 22:46:31 -0700

    Enhance "Watch your targets!" message

--- TranscendenceRL/SpaceObject/HPSystem.cs
+++ TranscendenceRL/SpaceObject/HPSystem.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using SadConsole;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +16,7 @@ namespace TranscendenceRL {
         }
         public void Damage(SpaceObject source, int hp) {
             if(source is PlayerShip ps) {
-                ps.AddMessage(new PlayerMessage($@"""Watch your targets!"" - {owner.Name}"));
+                ps.AddMessage(new PlayerMessage(new ColoredString($@"""Watch your targets!"" - {owner.Name}", Color.White, Color.Transparent), 1));
             }
             this.hp -= hp;
             if(this.hp < 1) {
