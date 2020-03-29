9ab0040f545e537d8a20ca2e86a71701b48790f6
Alex Chen <achen115@ucr.edu>
2020-03-28 22:46:09 -0700

    Implement title screen combat

--- TranscendenceRL/Screens/TitleConsole.cs
+++ TranscendenceRL/Screens/TitleConsole.cs
@@ -17,6 +17,11 @@ namespace TranscendenceRL {
     class TitleConsole : Window {
         string[] title = Properties.Resources.Title.Replace("\r\n", "\n").Split('\n');
         World World = new World();
+
+        public IShip pov;
+        public XY camera;
+        public Dictionary<(int, int), ColoredGlyph> tiles;
+
         public TitleConsole(int width, int height) : base(width, height) {
             UseKeyboard = true;
             ButtonTheme BUTTON_THEME = new SadConsole.Themes.ButtonTheme() {
@@ -26,6 +31,9 @@ namespace TranscendenceRL {
                 MouseDown = new Cell(Color.White, Color.Transparent),
                 MouseOver = new Cell(Color.Cyan, Color.Transparent),
             };
+            camera = new XY(0, 0);
+            tiles = new Dictionary<(int, int), ColoredGlyph>();
+            /*
             {
                 var size = 20;
                 var y = (Height * 3) / 4;
@@ -51,8 +59,10 @@ namespace TranscendenceRL {
                 exit.Click += (o, e) => Exit();
                 Add(exit);
             }
+            */
 
             World.types.Load(Directory.GetFiles("Content", "*.xml"));
+
         }
         private void StartGame() {
             Hide();
@@ -61,6 +71,70 @@ namespace TranscendenceRL {
         }
         private void Exit() {
             Environment.Exit(0);
+        }
+        public override void Update(TimeSpan timeSpan) {
+            World.entities.UpdateSpace();
+            World.effects.UpdateSpace();
+
+            tiles.Clear();
+            //Update everything
+            foreach (var e in World.entities.all) {
+                e.Update();
+                if (e.Tile != null && !tiles.ContainsKey(e.Position)) {
+                    tiles[e.Position] = e.Tile;
+                }
+            }
+            foreach (var e in World.effects.all) {
+                e.Update();
+                if (e.Tile != null && !tiles.ContainsKey(e.Position)) {
+                    tiles[e.Position] = e.Tile;
+                }
+            }
+
+            World.entitiesAdded.ForEach(e => World.entities.all.Add(e));
+            World.effectsAdded.ForEach(e => World.effects.all.Add(e));
+            World.entitiesAdded.Clear();
+            World.effectsAdded.Clear();
+            World.entities.all.RemoveWhere(e => !e.Active);
+            World.effects.all.RemoveWhere(e => !e.Active);
+
+            if(pov == null || !pov.Active) {
+                var shipClasses = World.types.shipClass.Values;
+                var shipClass = shipClasses.ElementAt(new Random().Next(shipClasses.Count));
+                var ship = new Ship(World, shipClass, Sovereign.Gladiator, new XY(-10, -10));
+                pov = new AIShip(ship, new AttackAllOrder(ship));
+                World.entities.all.Add(pov);
+            }
+
+            if(World.entities.all.Count < 2) {
+                var shipClasses = World.types.shipClass.Values;
+                var shipClass = shipClasses.ElementAt(new Random().Next(shipClasses.Count));
+                var angle = World.karma.NextDouble() * Math.PI * 2;
+                var distance = World.karma.Next(10, 20);
+                var ship = new Ship(World, shipClass, Sovereign.Gladiator, World.entities.all.First().Position + XY.Polar(angle, distance));
+                var enemy = new AIShip(ship, new AttackAllOrder(ship));
+                World.entities.all.Add(enemy);
+            }
+            /*
+            //The buffer is updated every update rather than every draw, so that the camera stays centered on the ship
+            tiles.Clear();
+            for (int x = 0; x < Width; x++) {
+                for (int y = 0; y < Height; y++) {
+                    var offset = new XY(x, y) - new XY(Width / 2, Height / 2);
+                    var location = camera + offset;
+                    var e = World.entities[location].FirstOrDefault() ?? World.effects[location].FirstOrDefault();
+                    if (e != null) {
+                        var tile = e.Tile;
+                        if (tile.Background == Color.Transparent) {
+                            tile.Background = World.backdrop.GetBackground(location, camera);
+                        }
+                        tiles[(x, y)] = tile;
+                    } else {
+                        tiles[(x, y)] = World.backdrop.GetTile(new XY(x, y), camera);
+                    }
+                }
+            }
+            */
         }
         public override void Draw(TimeSpan drawTime) {
             Clear();
@@ -82,13 +156,42 @@ namespace TranscendenceRL {
             titleY++;
             Print(titleX, titleY, new ColoredString(@"April 2020", Color.White, Color.Black));
 
+            titleY++;
+            titleY++;
+            Print(titleX, titleY, new ColoredString("[Enter]  Start", Color.White, Color.Black));
+            titleY++;
+            Print(titleX, titleY, new ColoredString("[Escape] Exit", Color.White, Color.Black));
+            camera = pov.Position;
             for (int x = 0; x < Width; x++) {
                 for (int y = 0; y < Height; y++) {
                     var g = GetGlyph(x, y);
                     if (g == 0 || g == ' ') {
-                        Print(x, y, World.backdrop.GetTileFixed(new XY(x, y)));
+                        
+                        var offset = new XY(x, y) - new XY(Width / 2, Height / 2);
+                        var location = camera + offset;
+                        if(tiles.TryGetValue(location, out var tile)) {
+                            if(tile.Background == Color.Transparent) {
+                                tile.Background = World.backdrop.GetBackground(location, camera);
+                            }
+                            Print(x, y, tile);
+                        } else {
+                            Print(x, y, World.backdrop.GetTile(location, camera));
+                        }
+                        
+                        /*
+                        var e = World.entities[location].FirstOrDefault() ?? World.effects[location].FirstOrDefault();
+                        if (e != null) {
+                            var tile = e.Tile;
+                            if(tile.Background == Color.Transparent) {
+                                tile.Background = World.backdrop.GetBackground(location, camera);
+                            }
+                            Print(x, y, tile);
+                        } else {
+                            Print(x, y, World.backdrop.GetTile(new XY(x, y), camera));
+                        }
+                        */
                     } else {
-                        SetBackground(x, y, World.backdrop.GetBackgroundFixed(new XY(x, y)));
+                        SetBackground(x, y, World.backdrop.GetBackground(new XY(x, y), camera));
                     }
                     
                 }
@@ -97,6 +200,12 @@ namespace TranscendenceRL {
             base.Draw(drawTime);
         }
         public override bool ProcessKeyboard(Keyboard info) {
+            if(info.IsKeyPressed(Enter)) {
+                StartGame();
+            }
+            if(info.IsKeyPressed(Escape)) {
+                Exit();
+            }
 #if DEBUG
             if (info.IsKeyDown(LeftShift) && info.IsKeyPressed(G)) {
                 Hide();
